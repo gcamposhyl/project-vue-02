6b9d8bcd7543b29cba5c51d2984072999d597bb4
Gabriel Campos <gcampos@hyl.cl>
2023-07-18 17:57:20 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from './components/Header.vue'
+import Formulario from './components/Formulario.vue'
+</script>
+
+<template>
+    <div class="container mx-auto mt-20">
+        <Header />
+    </div>
+  
+</template>
